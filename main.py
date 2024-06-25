aa8cc729d99933375e979d7a70d4c8323e943543
OrionRav <OrionRav@yandex.ru>
2024-06-25 05:57:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# Задача «Длина слова»
+a='Hello world!'  # присвоение переменной строки
+print(str(a))     # вывод текста строки
+print(len(str(a)))  # вычисление длины строки
+
+# Задача «Суммы и разности»
+first=17
+second=28
+summa=(int(first) + int(second))
+diff=(int(first) - int(second))
+print(summa, diff)
+
+# Задача «Среднее арифметическое»
+first=73
+second=85
+third=61
+mean=((int(first) + int(second) + int(third)) / 3)
+print(mean)
+
+# Задача «Простые строчки»
+first_string='Понедельник'
+second_string='Вторник'
+print(str(first_string) + ', ' + str(second_string))
+
+# Задача «Сложная формула»
+a=5
+b=7
+c=3
+f=((((int(a) * int(b)) + (int(a) * int(c))) ** 3) / 2)
+print(f)
